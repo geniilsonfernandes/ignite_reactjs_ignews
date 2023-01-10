0e078736d7bb3812601427bc2632da74c013b9af
genilson <genilson.fernandes@teamsoft.com.br>
2023-01-10 08:18:59 -0300

    add  fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -7,7 +9,25 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_SECRET_ID!,
       scope: "read:user",
-      
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email, name, image } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(name!)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(name!)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
---
+++ src/services/fauna.ts
@@ -0,0 +1,6 @@
+import { Client } from "faunadb";
+
+export const fauna = new Client({
+  secret: "fnAE58JGKzACVRO6hfFCb8GI3o-zn1HvpaXiUQCS",
+  domain: "db.us.fauna.com",
+});
